bc258c5edb25b0a89c3eefa5687a356e15823430
MiraHämäläinen <mira.m.hamalainen@gmail.com>
2023-01-14 16:22:17 +0200

    add error handler

--- DroneBirdBack/index.js
+++ DroneBirdBack/index.js
@@ -1,8 +1,10 @@
 const express = require('express')
+require('express-async-errors')
 const app = express()
 const { PORT } = require('./utils/config')
 const { connectToDatabase } = require('./utils/db')
 
+const middleware = require('./utils/middleware')
 const dronesRouter = require('./controllers/drones')
 const droneownersRouter = require('./controllers/droneowners')
 
@@ -10,6 +12,9 @@ app.use(express.json())
 
 app.use('/api/drones', dronesRouter)
 app.use('/api/droneowners', droneownersRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 const start = async () => {
   await connectToDatabase()
---
+++ DroneBirdBack/utils/middleware.js
@@ -0,0 +1,17 @@
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+const errorHandler = (error, request, response, next) => {
+  console.log(error.name)
+  console.log(error.message)
+
+  if (error) {
+    return response.status(400).send(error.message)
+  } else next(error)
+}
+
+module.exports = {
+  unknownEndpoint,
+  errorHandler
+}
