280fd9d8beacf2b36276669a49c64fe22af7947a
MiraHämäläinen <mira.m.hamalainen@gmail.com>
2023-01-14 13:08:09 +0200

    setup eslint + prettier rules

--- DroneBirdBack/index.js
+++ DroneBirdBack/index.js
@@ -4,8 +4,8 @@ const { PORT } = require('./utils/config')
 const { connectToDatabase } = require('./utils/db')
 
 app.get('/', (req, res) => {
-    res.send('<h1>Starting up!</h1>')
-  })
+  res.send('<h1>Starting up!</h1>')
+})
 
 const start = async () => {
   await connectToDatabase()
