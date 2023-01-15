3b85a696bdcf758aef8e1cc5d7fe40091411d1b8
MiraHämäläinen <mira.m.hamalainen@gmail.com>
2023-01-15 10:17:03 +0200

    Getting drone data from the back

--- drone-bird-front/src/App.js
+++ drone-bird-front/src/App.js
@@ -1,7 +1,31 @@
-function App() {
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+
+const App = () => {
+  const [drones, setDrones] = useState()
+  const getDrone = async () => {
+    try {
+      const droneData = await axios.get('/api/drones')
+      setDrones(droneData.data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  useEffect(() => {
+    getDrone()
+  }, [])
+
+  if (!drones)
+    return (
+      <div>
+        <h3>Loading...</h3>
+      </div>
+    )
+
   return (
     <div>
-      <h1>Getting started!</h1>
+      <h1>Got the data!</h1>
+      <p>{drones[0].serialNumber}</p>
     </div>
   )
 }
